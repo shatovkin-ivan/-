05ad1475161792ba726875d614b4669b721c6a0a
Anatoly Rykov <jollyroger2010@mail.ru>
2020-08-26 17:46:16 +0300

    6

--- task2-master/script/script.js
+++ task2-master/script/script.js
@@ -6,43 +6,43 @@ $(document).ready(function () {
       $('html:not(:animated),body:not(:animated)').animate({ scrollTop: dest }, 1000);
 	});
 
-// 	let desc = $('.awards__title');
-// 	let description = $('.awards__text');
+	let desc = $('.awards__title');
+	let description = $('.awards__text');
 
-// 	autoresize(description);
-// 	autoresize(desc);
-// })
+	autoresize(description);
+	autoresize(desc);
+ })
 
-// function autoresize (heightDescription) {
-// 	let reboot = 0;
+function autoresize (heightDescription) {
+	let reboot = 0;
 
-// 	$(heightDescription).each(function () {
-// 		let h = parseInt($(this).height());
-// 		if (h > reboot) {
-// 			reboot = h;
-// 			}
-// 		});
-// 	$(heightDescription).height(reboot);
+	$(heightDescription).each(function () {
+		let h = parseInt($(this).height());
+		if (h > reboot) {
+			reboot = h;
+			}
+		});
+	$(heightDescription).height(reboot);
+}
+
+
+// window.onload = () => {
+// 	const title = document.querySelectorAll('.awards__title');
+// 	const text = document.querySelectorAll('.awards__text');
+// 	res(title);
+// 	res(text);
 // }
-})
+// function res (heightText) {
+// 	let startValue = 0;
 
-
-window.onload = () => {
-	const title = document.querySelectorAll('.awards__title');
-	const text = document.querySelectorAll('.awards__text');
-
-	resize(title);
-	resize(text);
-}
-
-function resize (heightText) {
-	const startValue = 0;
-	console.log(heightText);
-	for(let i = 0; i < heightText.length; i++) {
-		const hk = heightText.height;
-		console.log(hk);
-	}
-}
+// 	for(let i = 0; i < heightText.length; i++) {
+// 		let h = heightText[i].offsetHeight;
+// 		if (h > startValue) {
+// 			startValue = h;
+// 		}
+// 		heightText[i].style.height = startValue + "px";
+// 	}
+// }
 // function autoResize(contentElements) {
 // 	for(let i = 0; i < contentElements.length; i++) {
 // 		let elementHeight = contentElements.offsetHeight;
